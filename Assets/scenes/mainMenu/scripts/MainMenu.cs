bad31a54f9690a83382be28438c125752d70e0b3
Mark McKenna <mmckenna91@gmail.com>
2014-03-21 23:42:18 -0400

    main menu background

---
+++ Assets/scenes/mainMenu/scripts/MainMenu.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MainMenu : MonoBehaviour {
+
+	public Texture backgroundTexture;
+
+	void OnGUI(){
+		//display background texture
+		GUI.DrawTexture(new Rect(0,0,Screen.width,Screen.height), backgroundTexture);
+	}
+}

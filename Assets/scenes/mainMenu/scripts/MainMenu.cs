1ba1efde2340070f8b8039a325e6f6410818ee92
Mark McKenna <mmckenna91@gmail.com>
2014-03-22 00:13:43 -0400

    main menu buttons

--- Assets/scenes/mainMenu/scripts/MainMenu.cs
+++ Assets/scenes/mainMenu/scripts/MainMenu.cs
@@ -8,5 +8,37 @@ public class MainMenu : MonoBehaviour {
 	void OnGUI(){
 		//display background texture
 		GUI.DrawTexture(new Rect(0,0,Screen.width,Screen.height), backgroundTexture);
+
+		//displays our buttons
+		if(GUI.Button(buttonRect(1), "Play Game")) {
+			print("Clicked play game");
+		}
+
+		if(GUI.Button(buttonRect(2), "Options")) {
+			print("Clicked options");
+		}
+		
+		if(GUI.Button(buttonRect(3), "Highscores")) {
+			print("Clicked highscores");
+		}
+		
+		if(GUI.Button(buttonRect(4), "Help")) {
+			print("Clicked help");
+		}
+		
+		if(GUI.Button(buttonRect(5), "Exit")) {
+			print("Clicked exit");
+		}
+	}
+
+	Rect buttonRect(int buttonNumber){
+		return new Rect(Screen.width * 0.25f, top(buttonNumber), Screen.width * 0.5f, Screen.height * 0.1f);
+	}
+
+	float top(int buttonNumber){
+		float buttonPlacementY = 0.2f;
+		float buttonOffsetY = 0.125f;
+
+		return Screen.height * (buttonPlacementY + buttonOffsetY * buttonNumber);
 	}
 }
